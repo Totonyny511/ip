8b3989a1868cead633f7cae08e2659d570c805e4
Tony <E1525405@u.nus.edu>
2026-08-19 11:52:03 +0800

    Update Duke.java with a new name to Tony.java

---
+++ src/main/java/Tony.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Tony chatbot application.
+ */
+public class Tony {
+    public static void main(String[] args) {
+        String banner = " _____   ___   _   _ __   __\n"
+                + "|_   _| / _ \\ | \\ | |\\ \\ / /\n"
+                + "  | |  | | | ||  \\| | \\ V /\n"
+                + "  | |  | |_| || |\\  |  | |\n"
+                + "  |_|   \\___/ |_| \\_|  |_|\n";
+        System.out.println(banner);
+    }
+}
