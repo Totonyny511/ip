e3de53fe14c6567ce0fdc8d269a6c63408ae3553
Tony <E1525405@u.nus.edu>
2026-08-19 12:10:29 +0800

    Update Tony.java with an echo functiona

--- src/main/java/Tony.java
+++ src/main/java/Tony.java
@@ -1,13 +1,41 @@
+import java.util.Scanner;
+
 /**
  * Starts the Tony chatbot application.
  */
 public class Tony {
+    /**
+     * Displays Tony's greeting, echoes each entered command, and exits when the
+     * user enters {@code bye}.
+     *
+     * @param args command-line arguments (not used by this application)
+     */
     public static void main(String[] args) {
         String banner = " _____   ___   _   _ __   __\n"
                 + "|_   _| / _ \\ | \\ | |\\ \\ / /\n"
                 + "  | |  | | | ||  \\| | \\ V /\n"
                 + "  | |  | |_| || |\\  |  | |\n"
-                + "  |_|   \\___/ |_| \\_|  |_|\n";
+                + "  |_|   \\___/ |_| \\_|  |_|";
         System.out.println(banner);
+        String line = "________________________________________________";
+
+        System.out.println(line);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(line);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(line);
+        }
     }
 }
