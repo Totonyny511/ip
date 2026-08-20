951ef8c5ad27b6c44aae92c30a399499c10a0f12
Tony <E1525405@u.nus.edu>
2026-08-21 00:37:51 +0800

    Update TaskType.java with enum implementation

---
+++ src/main/java/TaskType.java
@@ -0,0 +1,29 @@
+/**
+ * Represents the supported kinds of tasks and their list-display icons.
+ */
+public enum TaskType {
+    TODO("T"),
+    DEADLINE("D"),
+    EVENT("E");
+
+    /** The icon displayed before a task's completion status. */
+    private final String icon;
+
+    /**
+     * Creates a task type with its list-display icon.
+     *
+     * @param icon the letter shown for this type of task
+     */
+    TaskType(String icon) {
+        this.icon = icon;
+    }
+
+    /**
+     * Returns the icon used when displaying this task type.
+     *
+     * @return the task type icon
+     */
+    public String getIcon() {
+        return icon;
+    }
+}
